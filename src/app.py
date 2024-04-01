ecc7e492e6162cdfc73c91bba147828e18e0b660
peaceFrog <eric.paula@fatec.sp.gov.br>
2024-04-01 17:46:16 -0300

    teste

--- src/app.py
+++ src/app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template
 
 app = Flask("__name__")
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000, debug=True, threaded=True)
 
 @app.route("/")
 def home():
