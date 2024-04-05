7f1d40702fe4712c3e740d39276395365c35e914
peaceFrog <eric.paula@fatec.sp.gov.br>
2024-04-05 15:08:45 -0300

    Refazendo o site

--- src/app.py
+++ src/app.py
@@ -6,9 +6,17 @@ app = Flask("__name__")
 def home():
     return render_template('index.html', current_page='home')
 
+@app.route("/chart")
+def chart():
+    return render_template('grafico.html', current_page='chart')
+
 @app.route("/agua")
 def agua():
     return render_template('agua.html', current_page='agua')
+
+@app.route("/temp")
+def temp():
+    return render_template('temp.html', current_page='temp')
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000, debug=True, threaded=True)
