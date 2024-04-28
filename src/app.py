2616074b39a5c25eceee8655c8ef478846e0bc97
peaceFrog <eric.paula@fatec.sp.gov.br>
2024-04-27 22:45:44 -0300

    Comentando e organizando código

--- src/app.py
+++ src/app.py
@@ -1,47 +1,44 @@
 import pandas as pd
 import plotly.graph_objects as go
-import plotly.express as px
 from dash import dcc, html, Dash
 from flask import Flask, render_template
 
- 
 app = Flask(__name__)
-
-
 dash = Dash(__name__, server=app,url_base_pathname='/grafico/')
 
-dados=pd.read_csv('dadosensores_media.csv')
-dados['datahora']=pd.to_datetime(dados['data']+' '+dados['hora'])
-dados = dados.drop('dia_semana', axis=1)
-dados = dados.drop('data', axis=1)
-dados = dados.drop('hora', axis=1)
-desired_year = 2023
-desired_month = 10  # For example, September
+def puxa_dados():
+    dados=pd.read_csv('dadosensores_media.csv')
+    dados['datahora']=pd.to_datetime(dados['datahora'])
 
-# Filter the dataset to include only the rows corresponding to the desired year and month
-dados_filtered = dados[(dados['datahora'].dt.year == desired_year) & (dados['datahora'].dt.month == desired_month)]
+    # Seleciona Ano e Mês
+    desired_year = 2023
+    desired_month = 10 
 
-# Calculate hourly averages
-dados_filtered_hourly = dados_filtered.resample('d', on='datahora').mean().reset_index()
+    # Filtrando baseado no ano e mês
+    dados_filtered = dados[(dados['datahora'].dt.year == desired_year) & (dados['datahora'].dt.month == desired_month)]
 
-dados=dados_filtered_hourly
- 
-fig = go.Figure(data=[
-    go.Bar(name='Umidade Solo', x=dados['datahora'], y=dados['umidade_solo']),
-    go.Bar(name='Umidade Ambiente', x=dados['datahora'], y=dados['umidade_ambiente']),
-    go.Bar(name='Temperatura', x=dados['datahora'], y=dados['temperatura']),
-    go.Bar(name='Volume de Água', x=dados['datahora'], y=dados['volume_agua'])
-])
+    # Calculando média do dia ('d')
+    dados=dados_filtered.resample('d', on='datahora').mean().reset_index()
+    
+    # Puxando as colunas e adicionando barras baseado nos valores obtidos
+    fig = go.Figure(data=[
+        go.Bar(name='Umidade Solo', x=dados['datahora'], y=dados['umidade_solo']),
+        go.Bar(name='Umidade Ambiente', x=dados['datahora'], y=dados['umidade_ambiente']),
+        go.Bar(name='Temperatura', x=dados['datahora'], y=dados['temperatura']),
+        go.Bar(name='Volume de Água', x=dados['datahora'], y=dados['volume_agua'])
+    ])
 
-# Change the bar mode
-fig.update_layout(title="Data Analysis",
-                  xaxis_title="Data", yaxis_title="Valores", showlegend=True)
+    # Não sei, mas precisa disso
+    fig.update_layout(title="Data Analysis",
+                    xaxis_title="Data", yaxis_title="Valores", showlegend=True)
+    
+    return fig
 
 # Layout do Dash
 dash.layout = html.Div(children=[
     dcc.Graph(
         id='id_media',
-        figure=fig,
+        figure=puxa_dados(),
         config={'modeBarButtonsToRemove': ['zoom2d', 'pan2d', 'select2d', 'lasso2d', 'zoomIn2d', 'zoomOut2d', 'autoScale2d', 'resetScale2d', 'hoverClosestCartesian', 'hoverCompareCartesian']}
     ),
 ])
@@ -54,6 +51,7 @@ def home():
 
 @app.route("/grafico")
 def chart():
+    fig=puxa_dados()
     chart_html = fig.to_json()
     return render_template('grafico.html',chart=chart_html)
 
